33443841a1ed12498bc46f9e226717e9370af2ea
flyxiaozhu <1647622369@qq.com>
2018-11-07 15:45:38 +0800

    docker 搭建

---
+++ server-consumer/src/main/java/com/flyxiaozhu/serveradminconsumer/service/HelloService.java
@@ -0,0 +1,21 @@
+package com.flyxiaozhu.serveradminconsumer.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+    @Autowired
+    RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "hiError") // 熔断器
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://server-hello/hello?name=" + name, String.class);
+    }
+
+    public String hiError(String name) {
+        return "Hi," + name + ".sorry!!!error...";
+    }
+}
---
+++ server-feign/src/main/java/com/flyxiaozhu/serverfeign/service/HiService.java
@@ -0,0 +1,11 @@
+package com.flyxiaozhu.serverfeign.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "server-hello",fallback = HiServiceImpl.class)
+public interface HiService {
+    @RequestMapping(value = "/hello")
+    String sayLikeClientOne(@RequestParam(value = "name") String name);
+}
