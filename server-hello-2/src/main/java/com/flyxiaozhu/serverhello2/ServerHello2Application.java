3e431390566339b3ca04164091401474adee2080
flyxiaozhu <1647622369@qq.com>
2018-11-07 17:36:51 +0800

    整理 项目结构

---
+++ server-hello-2/src/main/java/com/flyxiaozhu/serverhello2/ServerHello2Application.java
@@ -0,0 +1,27 @@
+package com.flyxiaozhu.serverhello2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServerHello2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerHello2Application.class, args);
+	}
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/hello")
+    public String like(@RequestParam(value = "name") String name) {
+        return "Hello," + name + ".my port is:" + port;
+    }
+}
---
+++ server-hello/src/main/java/com/flyxiaozhu/serverhello/ServerHelloApplication.java
@@ -0,0 +1,37 @@
+package com.flyxiaozhu.serverhello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RefreshScope
+public class ServerHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerHelloApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${test}")
+    private String test;
+
+    @RequestMapping("/hello")
+    public String like(@RequestParam(value = "name") String name) {
+        return "Hello," + name + ".my port is:" + port;
+    }
+
+    @RequestMapping("/test")
+    public String test(){
+        return test;
+    }
+}
