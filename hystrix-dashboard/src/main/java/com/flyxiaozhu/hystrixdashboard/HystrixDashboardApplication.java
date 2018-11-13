594b98e136bb9a4df80a4684b9037b8b6fc628a2
flyxiaozhu <1647622369@qq.com>
2018-11-13 17:53:47 +0800

    修改 bug

--- hystrix-dashboard/src/main/java/com/flyxiaozhu/hystrixdashboard/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/flyxiaozhu/hystrixdashboard/HystrixDashboardApplication.java
@@ -1,4 +1,4 @@
-package com.flyxiaozhu.hystrixdashboardfeign;
+package com.flyxiaozhu.hystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +8,9 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrixDashboard // 启用 Hystrix Dashboard 功能
-public class HystrixDashboardFeignApplication {
+public class HystrixDashboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HystrixDashboardFeignApplication.class, args);
+		SpringApplication.run(HystrixDashboardApplication.class, args);
 	}
 }
