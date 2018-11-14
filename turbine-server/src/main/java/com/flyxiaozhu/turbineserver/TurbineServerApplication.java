713d7a461af878792102b8036958f9ca1c2f859d
flyxiaozhu <1647622369@qq.com>
2018-11-14 11:38:45 +0800

    测试 bug

--- turbine-server/src/main/java/com/flyxiaozhu/turbineserver/TurbineServerApplication.java
+++ turbine-server/src/main/java/com/flyxiaozhu/turbineserver/TurbineServerApplication.java
@@ -3,11 +3,13 @@ package com.flyxiaozhu.turbineserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableTurbine  //开启 Turbine
+@EnableHystrixDashboard
 public class TurbineServerApplication {
 
 	public static void main(String[] args) {
